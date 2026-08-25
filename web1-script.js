93f3482328aee8b87916145e0aa14e0d9dea3744
nadineabbas22 <nadinejannoun147@gmail.com>
2026-08-25 03:44:54 +0300

    Add book filtering and borrowing functionality

--- web1-script.js
+++ web1-script.js
@@ -40,4 +40,85 @@ searchInput.addEventListener("input", function () {
 
             card.style.display = "none";
 
-        }
+        }  });
+
+});
+
+// Filter books by category
+
+const categoryButtons =
+
+    document.querySelectorAll(".category-btn");
+
+categoryButtons.forEach(function (button) {
+
+    button.addEventListener("click", function () {
+
+        const selectedCategory =
+
+            button.getAttribute("data-category");
+
+        // Change active button
+
+        categoryButtons.forEach(function (btn) {
+
+            btn.classList.remove("active");
+
+        });
+
+        button.classList.add("active");
+
+        // Filter cards
+
+        bookCards.forEach(function (card) {
+
+            const cardCategory =
+
+                card.getAttribute("data-category");
+
+            if (
+
+                selectedCategory === "all" ||
+
+                cardCategory === selectedCategory
+
+            ) {
+
+                card.style.display = "block";
+
+            } else {
+
+                card.style.display = "none";
+
+            }
+
+        });
+
+    });
+
+});
+
+// Borrow Books
+
+const borrowButtons =
+
+    document.querySelectorAll(".borrow-btn");
+
+const borrowedCount =
+
+    document.getElementById("borrowedCount");
+
+let borrowedBooks = 0;
+
+borrowButtons.forEach(function (button) {
+
+    button.addEventListener("click", function () {
+
+        if (button.textContent === "Borrow") {
+
+            borrowedBooks++;
+
+            button.textContent = "Borrowed";
+
+            button.style.backgroundColor = "#777";
+                }else{
